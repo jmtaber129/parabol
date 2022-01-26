f33ab09c87b90d6634a3abdc8091bae2b76cc8e4
James Taber <jmtaber129@gmail.com>
2022-01-25 23:05:43 -0500

    Fix lint-staged / yarn lint

--- packages/client/lint-staged.config.js
+++ packages/client/lint-staged.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  'package/client/**/!(*graphql).{js,ts,tsx}': 'eslint --fix',
+  '**/!(*graphql).{ts,tsx}': 'eslint --fix',
   '**/*.{ts,tsx}': () => 'tsc --noEmit -p tsconfig.json'
 }
